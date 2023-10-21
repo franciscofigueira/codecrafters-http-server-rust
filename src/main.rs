891a3e63048ab585fb895a2d287d42d97914ae40
franciscofigueira <francisco.gomes.figueira@proton.me>
2023-10-21 22:13:00 +0100

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -10,10 +10,27 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                _stream
-                    .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .expect("should write to stream");
-                _stream.flush().expect("flush stream");
+                let mut buf = [0; 128];
+                _stream.read(&mut buf).unwrap();
+                let request = std::str::from_utf8(&buf).unwrap();
+
+                let request: Vec<_> = request.split(" ").collect();
+                println!("{:?}", request.clone());
+                if request[1] == "/" {
+                    _stream
+                        .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                        .expect("should write to stream");
+                    _stream.flush().expect("flush stream");
+                } else {
+                    _stream
+                        .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                        .expect("should write to stream");
+                    _stream.flush().expect("flush stream");
+                }
+                // _stream
+                //     .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                //     .expect("should write to stream");
+                // _stream.flush().expect("flush stream");
             }
             Err(e) => {
                 println!("error: {}", e);
